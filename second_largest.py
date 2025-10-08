8c7357cc01600547110e950d934244eee77bf524
DHIYANSHU <chillbrofreefiredev123@gmail.com>
2025-10-08 15:49:22 +0800

    Create second_largest.py

---
+++ second_largest.py
@@ -0,0 +1,10 @@
+# second_largest.py
+
+def second_largest(nums):
+    unique_nums = list(set(nums))
+    if len(unique_nums) < 2:
+        return None
+    unique_nums.sort()
+    return unique_nums[-2]
+
+print(second_largest([10, 20, 4, 45, 99]))
